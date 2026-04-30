aaecd66ebf60ae1bcabe521ee69c2182e5ef8752
Ocupuc123 <ocupuc123@mail.ru>
2026-04-30 09:52:21 +0300

    8.14. Открывается и закрывается

--- js/data.js
+++ js/data.js
@@ -86,6 +86,6 @@ const createPhoto = (index) => ({
   comments: getRandomComments(),
 });
 
-const createPhotos = Array.from({ length: PHOTOS_COUNT }, (_, index) => createPhoto(index + 1));
+const photos = Array.from({ length: PHOTOS_COUNT }, (_, index) => createPhoto(index + 1));
 
-export { createPhotos };
+export { photos };
